3e07adc1c72bd979603e2920f8534c2d9cff90b7
Dillon Powers <dillon.c.powers@gmail.com>
2016-11-04 23:36:42 -0400

    currentProduct component prettier and also receiving associated reviews

--- app/components/CurrentProduct.jsx
+++ app/components/CurrentProduct.jsx
@@ -1,17 +1,35 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { Image } from 'react-bootstrap';
 
 /* -----------------    COMPONENT     ------------------ */
 
 const CurrentProduct = ({ currentProduct }) => (
-	<div>
-		<h1>Component loaded!</h1>
-		<h3>{ currentProduct.name }</h3>
-		<img src="/images/default.jpg"></img>
-		<h4>{ currentProduct.price }</h4>
-		<p>{ currentProduct.description }</p>
+	<div id="currentProduct">
+		<photo>
+			<Image className="mainPhoto" src="/images/default.jpg" responsive />
+		</photo>
+		<info>
+			<h3>{ currentProduct.name }</h3>
+			<h5>SKU { currentProduct.sku }</h5>
+			<h4>${ currentProduct.price && currentProduct.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',') }</h4>
+			<form>
+				<select name="dropdown">
+					<option value="1" defaultValue>1</option>
+					<option value="2">2</option>
+					<option value="2">3</option>
+				</select>
+			</form>
+			<button>Add To Cart</button>
+		</info>
+		<description>
+			<h4>PRODUCT DETAILS</h4>
+			<p>{ currentProduct.description }</p>
+		</description>
+		<reviews>
+			<div>Number of reviews: { currentProduct.reviews && currentProduct.reviews.length }</div>
+		</reviews>
 	</div>
-
 )
 
 
