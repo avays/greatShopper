dafbbae3fe48c7303be96339e844bda831acabf1
Dillon Powers <dillon.c.powers@gmail.com>
2016-11-09 01:11:01 -0500

    made checkout pretty

--- app/components/Checkout.jsx
+++ app/components/Checkout.jsx
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import { Panel, Button, Col, Row } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap';
+
 /* -----------------    COMPONENT     ------------------ */
 
 class Cart extends React.Component {
 	render(){
 		return (
-			<div className="comp-container">
+			<div className="comp-container" id="checkout-banner">
 				<h2>Checkout</h2>
-				<div>Items in your cart: { this.props.cart && this.props.cart.length }</div>
+				<div>
+					<h4>Items in your cart: </h4>
+				{ 
+					this.props.cart && this.props.cart.map((item, index) => {
+						const title = (<h3>{item.product.name}</h3>)
+					  return (
+					  <div key={index}>
+					  <Row>
+					    <Panel header={title}>
+					      <Col sm={4}>
+						      <h5>Item price: ${item.product.price.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',')}</h5>
+						      <h5>Quantity: {item.quantity}</h5>
+						     </Col>
+						     <Col sm={4}>
+						      <LinkContainer to={`/product/${item.product.sku}`}>
+						      	<Button>View Product</Button>
+						      </LinkContainer>
+					      </Col>
+					    </Panel>
+					  </Row>
+					  </div>
+					  )
+					}) 
+				}
+
+				</div>
 				{ this.props.children }
 			</div>
 		);
