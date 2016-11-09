39dc1b24878bbe6126f110dafe1b2b4d9a42b5da
Dillon Powers <dillon.c.powers@gmail.com>
2016-11-09 00:23:07 -0500

    payment form looks nicer

--- app/components/Payment.jsx
+++ app/components/Payment.jsx
@@ -13,10 +13,10 @@ var Payment = React.createClass({
     mixins: [ReactScriptLoaderMixin],
     
     getInitialState: function() {
-    	return {
-    		scriptLoading: true,
-    		scriptLoadError: false
-    	};
+       return {
+          scriptLoading: true,
+          scriptLoadError: false
+       };
     },
 
     getScriptURL: function() {
@@ -25,49 +25,49 @@ var Payment = React.createClass({
 
     onScriptLoaded: function() {
       this.setState({ scriptLoading: false });
-     	Stripe.setPublishableKey(process.env.STRIPE_TEST_PUBLISHABLE_SECRET);
+        Stripe.setPublishableKey(process.env.STRIPE_TEST_PUBLISHABLE_SECRET);
     },
 
     onScriptError: function() {
-    	this.setState({ scriptLoading: false, scriptLoadError: true })
+       this.setState({ scriptLoading: false, scriptLoadError: true })
     },
 
     sendPayment: function(evt) {
-    	evt.preventDefault();
-    	var number = evt.target.number.value;
-    	var exp_month = evt.target.exp_month.value;
-    	var exp_year = evt.target.exp_year.value;
-    	var cvc = evt.target.cvc.value;
-    	Stripe.card.createToken({
-    		number,
-    		cvc,
-    		exp_month,
-    		exp_year
-    	}, this.stripeResponseHandler)
+       evt.preventDefault();
+       var number = evt.target.number.value;
+       var exp_month = evt.target.exp_month.value;
+       var exp_year = evt.target.exp_year.value;
+       var cvc = evt.target.cvc.value;
+       Stripe.card.createToken({
+          number,
+          cvc,
+          exp_month,
+          exp_year
+       }, this.stripeResponseHandler)
     },
 
     stripeResponseHandler: function(status, response) {
         if (response.error) {
             console.log('STRIPE ERROR', response.error);
         } else {
-    		console.log('STRIPE RESPONSE: ', response);
-    		var token = response.id;
-    		browserHistory.push(`/checkout/confirmation/${token}`);
-	   }
+          console.log('STRIPE RESPONSE: ', response);
+          var token = response.id;
+          browserHistory.push(`/checkout/confirmation/${token}`);
+      }
     },
 
     render: function() {
       return (
-     		<div className="comp-container">
-	     		{
-	     			(this.state.scriptLoading) ?
-	     			<h3>Payment form loading...</h3>
+           <div className="comp-container">
+              {
+                 (this.state.scriptLoading) ?
+                 <h3>Payment form loading...</h3>
                     :
-	     			<div>
-        				<h2>Enter payment info</h2>
+                 <div>
+                    <h2>Enter payment info</h2>
                       <Form horizontal onSubmit={this.sendPayment}>
                       <FormGroup controlId="formHorizontalCardNumber">
-                        <Col componentClass={ControlLabel} sm={2}>
+                        <Col componentClass={ControlLabel} sm={4}>
                           Card Number
                         </Col>
                         <Col sm={6}>
@@ -76,28 +76,54 @@ var Payment = React.createClass({
                       </FormGroup>
 
                       <FormGroup controlId="formHorizontalExpiration">
-                        <Col componentClass={ControlLabel} sm={2}>
+                        <Col componentClass={ControlLabel} sm={4}>
                           Expiration
                         </Col>
-                        <Col sm={2}>
-                            <FormControl name="exp_month" type="text" pattern="\d{2}" placeholder="MM" data-stripe="exp_month" />
-                            <FormControl name="exp_year" type="text" pattern="\d{2}" placeholder="YY" data-stripe="exp_month" />
+                        <Col sm={6}>
+                          <FormControl name="exp_month" componentClass="select" placeholder="Month" data-stripe="exp_month">
+                            <option>Month</option>
+                            <option value="01">Jan (01)</option>
+                            <option value="02">Feb (02)</option>
+                            <option value="03">Mar (03)</option>
+                            <option value="04">Apr (04)</option>
+                            <option value="05">May (05)</option>
+                            <option value="06">Jun (06)</option>
+                            <option value="07">Jul (07)</option>
+                            <option value="08">Aug (08)</option>
+                            <option value="09">Sep (09)</option>
+                            <option value="10">Oct (10)</option>
+                            <option value="11">Nov (11)</option>
+                            <option value="12">Dec (12)</option>
+                          </FormControl>
+
+                          <FormControl name="exp_year" componentClass="select" placeholder="Year" data-stripe="exp_year">
+                            <option>Year</option>
+                            <option value="16">2016</option>
+                            <option value="17">2017</option>
+                            <option value="18">2018</option>
+                            <option value="19">2019</option>
+                            <option value="20">2020</option>
+                            <option value="21">2021</option>
+                            <option value="22">2022</option>
+                            <option value="23">2023</option>
+                          </FormControl>
+
                         </Col>
                       </FormGroup>
 
                       <FormGroup controlId="formHorizontalCVC">
-                        <Col componentClass={ControlLabel} sm={2}>
+                        <Col componentClass={ControlLabel} sm={4}>
                           cvc
                         </Col>
-                        <Col sm={4}>
+                        <Col sm={6}>
                           <FormControl name="cvc" type="password" pattern="\d{3}" placeholder="CVC"  data-stripe="cvc" />
                         </Col>
                       </FormGroup>
                       <Button type="submit">Next: Confirmation</Button>
                       </Form>
                     </div>
-	   		}  
-    		  <div>
+            }  
+            <div>
                 <p> You will be able to review your order on the next screen</p>
               </div>
             </div>
@@ -108,34 +134,4 @@ var Payment = React.createClass({
 
 export default Payment;
 
-
-
-
-    // <form action="/your-charge-code" method="POST" id="payment-form" onSubmit={this.sendPayment}>
-    //   <span className="payment-errors"></span>
-
-    //   <div className="form-row">
-    //     <label>
-    //       <span>Card Number</span>
-    //       <input name="number" type="text" size="20" data-stripe="number" />
-    //     </label>
-    //   </div>
-
-    //   <div className="form-row">
-    //     <label>
-    //       <span>Expiration (MM/YY)</span>
-    //       <input name="exp_month" type="text" size="2" data-stripe="exp_month" />
-    //     </label>
-    //     <span> / </span>
-    //     <input name="exp_year" type="text" size="2" data-stripe="exp_year" />
-    //   </div>
-
-    //   <div className="form-row">
-    //     <label>
-    //       <span>CVC</span>
-    //       <input name="cvc" type="text" size="4" data-stripe="cvc" />
-    //     </label>
-    //   </div>
-    //   <input type="submit" className="submit" value="Submit Payment" />
-    // </form>
 
